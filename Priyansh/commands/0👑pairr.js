77c847a9d50402f6a5b9bf659065cddb0a0c742c
tirkumar58-sketch <tirkumar58@gmail.com>
2026-02-07 12:20:55 +0530

    Update 0👑pairr.js

--- Priyansh/commands/0👑pairr.js
+++ Priyansh/commands/0👑pairr.js
@@ -108,10 +108,7 @@ module.exports.run = async function ({ api, event, Users }) {
 
 ╚═══❖••° °••❖═══╝
 
-   ✶⊶⊷⊷❍⊶⊷⊷✶
-    ∩_∩  
- （„• ֊ •„)♡  
-╭─∪∪────♡──────⟡  │${name1}\n├───────♡──────⟡                           │${name2}\n╰───────♡──────⟡  `,
+  ✶⊶⊷⊷❍⊶⊷⊷✶ ${name1}   ✶⊶⊷⊷❍⊶⊷⊷✶ ${name2}`,
       mentions: [{ id: id2, tag: name2 }],
       attachment: fs.createReadStream(outPath)
     },
