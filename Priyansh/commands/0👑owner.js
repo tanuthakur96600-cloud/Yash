5ca191fb00acb73fa46009a217d11cdd19b552f9
tanuthakur96600-cloud <tanuthakur96600@gmail.com>
2026-02-20 16:31:52 +0545

    Update 0👑owner.js

--- Priyansh/commands/0👑owner.js
+++ Priyansh/commands/0👑owner.js
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
      react.includes("virat") || 
 react.includes("prefix")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆 ★\n\n✦🌸===『*★🌸✦\n\n ✦𝐅𝐞𝐜𝐞𝐛𝐨𝐨𝐤 𝐥𝐢𝐧𝐤\n 𝐌𝐞𝐫𝐞 𝐛𝐨𝐬𝐬 𝐤𝐚 𝐋𝐢𝐧𝐤😁😋 \n\n https://www.facebook.com/profile.php?id=61587052583313",
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆 ★\n\n✦🌸===『*★🌸✦\n\n ✦𝐅𝐞𝐜𝐞𝐛𝐨𝐨𝐤 𝐥𝐢𝐧𝐤\n 𝐌𝐞𝐫𝐞 𝐛𝐨𝐬𝐬 𝐤𝐚 𝐋𝐢𝐧𝐤😁😋 \n\n https://www.facebook.com/profile.php?id=61587312556844",
 				attachment: fs.createReadStream(__dirname + `/noprefix/owner.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
